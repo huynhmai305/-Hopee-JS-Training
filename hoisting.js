a34db9c1b4c7e164e370f0d13c859865f0b38de3
huynhmai305 <huynhmai305@gmail.com>
2019-04-04 10:03:56 +0700

    ngày 2/4

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+var main=function(){
+    for(var x=0;x<6;x++)
+    {
+        console.log(x);
+    }
+    console.log("value x is: "+x)
+}
+main()
---
+++ lambda.js
@@ -0,0 +1,4 @@
+var msg=()=>console.log("hello")
+msg()
+var num=(x)=>x*5
+console.log(num(3))
